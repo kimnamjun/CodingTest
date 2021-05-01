a4c7755f95c5be6769c3c6cdef1d3c59574bf0ab
vxqwe0 <vxqwe0@gmail.com>
2021-05-01 23:47:45 +0900

    ref

--- 레퍼런스/disjoint_set.py
+++ 레퍼런스/disjoint_set.py
@@ -1,29 +1,27 @@
-# 두 노드 합하기
-def union(a, b):
-    fa = find(a)
-    fb = find(b)
-    if fa != fb:
-        nodes[fb] = fa
-
-
 # 최상위 부모노드 반환
 def find(a):
     if a != nodes[a]:
         nodes[a] = find(nodes[a])
     return nodes[a]
 
+# 두 노드 합하기
+def union(a, b):
+    fa, fb = find(a), find(b)
+    if fa != fb:
+        nodes[fb] = fa
+
 
 # index: num, value: parent
 nodes = [i for i in range(5)]
 
 # 노드 연결
-links = [[0,1],[3,4],[2,3],[0,4]]
+links = [[0, 1], [3, 4], [2, 3], [0, 4]]
 for a, b in links:
     union(a, b)
 
 # 연산 마무리
-for i in nodes:
-    find(i)
+for idx in range(len(nodes)):
+    find(idx)
 
 # 출력
 for idx, val in enumerate(nodes):
